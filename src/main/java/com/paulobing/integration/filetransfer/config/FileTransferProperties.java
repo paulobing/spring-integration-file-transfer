2d861750d06a79e758c1a8d00e642b32fa3ad1f9
Paulo Bing <paulo.bing@gmail.com>
2026-05-21 18:32:18 +0200

    Initial Spring Integration file transfer pipeline

--- src/main/java/com/paulobing/integration/filetransfer/config/FileTransferProperties.java
+++ src/main/java/com/paulobing/integration/filetransfer/config/FileTransferProperties.java
@@ -1,5 +1,24 @@
 package com.paulobing.integration.filetransfer.config;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "file.transfer")
+@Validated
+@Data
 public class FileTransferProperties {
 
+    @NotBlank
+    private String sourceDir;
+
+    @NotBlank
+    private String targetDir;
+
+    @Min(1)
+    private long pollIntervalMillis;
 }
--- src/main/java/com/paulobing/integration/filetransfer/flow/FileTransferFlow.java
+++ src/main/java/com/paulobing/integration/filetransfer/flow/FileTransferFlow.java
@@ -1,5 +1,63 @@
 package com.paulobing.integration.filetransfer.flow;
 
+import java.io.File;
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.integration.dsl.IntegrationFlow;
+
+import com.paulobing.integration.filetransfer.config.FileTransferProperties;
+
+@Configuration
 public class FileTransferFlow {
 
+    @Bean
+    public MessageChannel fileTransferChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public FileReadingMessageSource fileReadingMessageSource(FileTransferProperties props) {
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setDirectory(new File(props.getSourceDir()));
+        source.setFilter(new AcceptOnceFileListFilter<>());
+        return source;
+    }
+
+    @Bean
+    public MessageHandler fileWritingMessageHandler(FileTransferProperties props) {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(
+            new File(props.getTargetDir())
+        );
+        handler.setAutoCreateDirectory(true);
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+    @Bean
+    public PollerMetadata poller(FileTransferProperties props) {
+        PollerMetadata poller = new PollerMetadata();
+        poller.setTrigger(new PeriodicTrigger(Duration.ofMillis(props.getPollIntervalMillis())));
+        return poller;
+    }
+
+    @Bean
+    public IntegrationFlow fileMoveFlow(FileReadingMessageSource fileReadingMessageSource,
+                                        MessageHandler fileWritingMessageHandler,
+                                        PollerMetadata poller) {
+        return IntegrationFlow
+            .from(fileReadingMessageSource, config -> config.poller(poller))
+            .channel(fileTransferChannel())
+            .handle(fileWritingMessageHandler)
+            .get();
+    }
 }
